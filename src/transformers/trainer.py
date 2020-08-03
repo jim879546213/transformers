d8e0ca52d547b41b69de736207a324f68d3a49c3
jim879546213 <jim879546213@gmail.com>
2020-08-03 12:19:58 +0800

    Update trainer.py
    根據https://github.com/huggingface/transformers/issues/5347

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -31,6 +31,7 @@ from .trainer_utils import (
     set_seed,
 )
 from .training_args import TrainingArguments
+from torch.utils.data.dataset import IterableDataset
 
 
 _use_native_amp = False
@@ -241,25 +242,25 @@ class Trainer:
             )
 
     def get_train_dataloader(self) -> DataLoader:
-        """
-        Returns the training :class:`~torch.utils.data.DataLoader`.
-
-        Will use no sampler if :obj:`self.train_dataset` is a :obj:`torch.utils.data.IterableDataset`, a random sampler
-        (adapted to distributed training if necessary) otherwise.
-
-        Subclass and override this method if you want to inject some custom behavior.
-        """
         if self.train_dataset is None:
             raise ValueError("Trainer: training requires a train_dataset.")
-        train_sampler = self._get_train_sampler()
-
-        return DataLoader(
+        if is_torch_tpu_available():
+            train_sampler = get_tpu_sampler(self.train_dataset)
+        else:
+            train_sampler = (
+                RandomSampler(self.train_dataset)
+                if self.args.local_rank == -1
+                else DistributedSampler(self.train_dataset)
+            )
+            
+        data_loader = DataLoader(
             self.train_dataset,
             batch_size=self.args.train_batch_size,
-            sampler=train_sampler,
+            sampler=train_sampler if not isinstance(self.train_dataset, IterableDataset) else None,
             collate_fn=self.data_collator,
-            drop_last=self.args.dataloader_drop_last,
         )
+
+        return data_loader
 
     def _get_eval_sampler(self, eval_dataset: Dataset) -> Optional[torch.utils.data.sampler.Sampler]:
         if isinstance(eval_dataset, torch.utils.data.IterableDataset):
